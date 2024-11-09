6bafb1bf3ad9f889aea8d826b3b5ab3e6d9d5941
lev tarasov <Levil6.6.4@yandex.ru>
2024-11-09 12:11:01 +0500

    fix: current right eth interface

--- STM32CubeIDE/CM7/Debug/Application/User/Startup/startup_stm32h745zitx.d
+++
@@ -1,2 +0,0 @@
-Application/User/Startup/startup_stm32h745zitx.o: \
- ../Application/User/Startup/startup_stm32h745zitx.s
--- STM32CubeIDE/CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/Levil/GitHub/projectPP2024/STM32H745ZI-Q-Webserver/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/Levil/GitHub/projectPP2024/STM32H745ZI-Q-Webserver/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/Levil/GitHub/projectPP2024/STM32H745ZI-Q-Webserver/Drivers/BSP/Components/lan8742/lan8742.h:
