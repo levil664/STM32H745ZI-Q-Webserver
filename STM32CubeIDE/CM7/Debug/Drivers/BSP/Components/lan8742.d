241b13d1be57ae2dcc16fbadf8649b31e23dcc13
lev tarasov <Levil6.6.4@yandex.ru>
2024-11-09 11:47:09 +0500

    feat: eth interface

---
+++ STM32CubeIDE/CM7/Debug/Drivers/BSP/Components/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/lan8742.o: \
+ C:/Users/Levil/GitHub/projectPP2024/projectPP2024/Drivers/BSP/Components/lan8742/lan8742.c \
+ C:/Users/Levil/GitHub/projectPP2024/projectPP2024/Drivers/BSP/Components/lan8742/lan8742.h
+C:/Users/Levil/GitHub/projectPP2024/projectPP2024/Drivers/BSP/Components/lan8742/lan8742.h:
