fe42b40587c7ba6f7bf99b0b3af00937b1138eeb
lev tarasov <Levil6.6.4@yandex.ru>
2024-12-06 15:59:09 +0500

    changed the server location from flash memory to microsd card

---
+++ CM7/Debug/Drivers/BSP/Components/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/lan8742.o: \
+ C:/Users/Levil/GitHub/projectPP2024/STM32H745_Nucleo_M7_ETH/Drivers/BSP/Components/lan8742/lan8742.c \
+ C:/Users/Levil/GitHub/projectPP2024/STM32H745_Nucleo_M7_ETH/Drivers/BSP/Components/lan8742/lan8742.h
+C:/Users/Levil/GitHub/projectPP2024/STM32H745_Nucleo_M7_ETH/Drivers/BSP/Components/lan8742/lan8742.h:
---
+++ CM7/Debug/Middlewares/Third_Party/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+Middlewares/Third_Party/FreeRTOS/stream_buffer.o: \
+ C:/Users/Levil/GitHub/projectPP2024/STM32H745_Nucleo_M7_ETH/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ ../../Middlewares/Third_Party/FreeRTOS/Source/include/stream_buffer.h
+../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+../../Middlewares/Third_Party/FreeRTOS/Source/include/stream_buffer.h:
